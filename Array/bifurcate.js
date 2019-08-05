7a53e0414beed9845f6f7fcefe0ea1d1fc298d07
fuheideMayuyu <xiaguangding@163.com>
2019-08-05 11:24:05 +0800

    筛选数组元素

--- Array/bifurcate.js
+++ Array/bifurcate.js
@@ -6,4 +6,15 @@ const bifurcate = (arr, filter) => arr.reduce((acc, val, i) => (
 // Reduce: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
 // acc: 累计器， val:当前值， i:当前索引
-// console.log(bifurcate(['beep', 'boop', 'boo', 'bar'], [true, true, false, true]))
+
+// 例：
+bifurcate(['beep', 'boop', 'foo', 'bar'], [true, true, false, true]); 
+console.log(bifurcate(['beep', 'boop', 'boo', 'bar'], [true, false, false, true]))
+
+
+const bifurcateBy = (arr, fn) =>
+  arr.reduce((acc, val, i) => (acc[fn(val, i) ? 0 : 1].push(val), acc), [[], []]);
+// 例：
+
+bifurcateBy(['beep', 'boop', 'foo', 'bar'], x => x[0] === 'b'); // [ ['beep', 'boop', 'bar'], ['foo'] ]
+  
