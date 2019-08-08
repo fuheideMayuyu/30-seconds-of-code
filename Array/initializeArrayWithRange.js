ce6a2ae427c7895defd6d1030be71c444b1500fd
fuheideMayuyu <xiaguangding@163.com>
2019-08-08 11:32:58 +0800

    创建一个可以给定范围的数组

---
+++ Array/initializeArrayWithRange.js
@@ -0,0 +1,7 @@
+// 创建一个可以给定范围的数组
+const initializeArrayWithRange = (end, start = 0, step = 1) =>
+  Array.from({ length: Math.ceil((end - start + 1) / step) }, (v, i) => i * step + start)
+
+  initializeArrayWithRange(5); // [0,1,2,3,4,5]
+  initializeArrayWithRange(7, 3); // [3,4,5,6,7]
+  initializeArrayWithRange(9, 0, 2); // [0,2,4,6,8]
