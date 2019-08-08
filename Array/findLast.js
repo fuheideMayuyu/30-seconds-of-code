d6a25f6fc755455b3397fc88688507a31bbd278c
fuheideMayuyu <xiaguangding@163.com>
2019-08-08 10:45:58 +0800

    数组嵌套层数控制

--- Array/findLast.js
+++ Array/findLast.js
@@ -4,3 +4,12 @@ const findLast = (arr, fn) => arr.filter(fn).pop()
 
 // 例:
 findLast([1, 2, 3, 4], n => n % 2 === 1)
+
+const findLastIndex = (arr, fn) =>
+  arr
+    .map((val, i) => [i, val])
+    .filter(([i, val]) => fn(val, i, arr))
+    .pop()[0];
+
+    findLastIndex([1, 2, 3, 4], n => n % 2 === 1)
+    console.log(findLastIndex([1, 2, 3, 4,5], n => n % 2 === 1))
