500154137f4d4b58b3b54ab31607a087286d84df
fuheideMayuyu <xiaguangding@163.com>
2019-08-08 11:34:36 +0800

    创建一个由大到小的自定义规则数组

--- Array/initializeArrayWithRange.js
+++ Array/initializeArrayWithRange.js
@@ -5,3 +5,14 @@ const initializeArrayWithRange = (end, start = 0, step = 1) =>
   initializeArrayWithRange(5); // [0,1,2,3,4,5]
   initializeArrayWithRange(7, 3); // [3,4,5,6,7]
   initializeArrayWithRange(9, 0, 2); // [0,2,4,6,8]
+
+
+
+const initializeArrayWithRangeRight = (end, start = 0, step = 1) =>
+  Array.from({ length: Math.ceil((end + 1 - start) / step) }).map(
+    (v, i, arr) => (arr.length - i - 1) * step + start
+  );
+
+initializeArrayWithRangeRight(5); // [5,4,3,2,1,0]
+initializeArrayWithRangeRight(7, 3); // [7,6,5,4,3]
+initializeArrayWithRangeRight(9, 0, 2); // [8,6,4,2,0]
