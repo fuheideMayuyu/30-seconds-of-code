c1e9d67723425981f690daa0859a3497feab725b
fuheideMayuyu <xiaguangding@163.com>
2019-08-09 10:16:55 +0800

    使用map处理数组

---
+++ Array/mapObject.js
@@ -0,0 +1,9 @@
+// 使用map处理数组
+
+const mapObject = (arr, fn) =>
+  (a => (
+    (a = [arr, arr.map(fn)]), a[0].reduce((acc, val, ind) => ((acc[val] = a[1][ind]), acc), {})
+  ))()
+
+  const squareIt = arr => mapObject(arr, a => a * a);
+squareIt([1, 2, 3]); // { 1: 1, 2: 4, 3: 9 }
