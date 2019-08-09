20a795d2ac60ee648d27b96242d6b34ce5dbe8f2
fuheideMayuyu <xiaguangding@163.com>
2019-08-09 10:26:28 +0800

    找出数组最大或最小的数

--- Array/maxN.js
+++ Array/maxN.js
@@ -4,3 +4,9 @@ const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n)
 
 maxN([1, 2, 3]); // [3]
 maxN([1, 2, 3], 2); // [3,2]
+
+// 找出最小的数
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n)
+
+minN([1, 2, 3]); // [1]
+minN([1, 2, 3], 2); // [1,2]
